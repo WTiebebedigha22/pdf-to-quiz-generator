ab29b2a7e349a9b1531e089c8f068ec093a42b03
clintt-09 <aisosaclinton09@gmail.com>
2025-01-28 11:55:51 +0100

    added some packages and configured prettier

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -1,30 +1,38 @@
-import "./globals.css";
-import { Metadata } from "next";
-import { Toaster } from "sonner";
-import { ThemeProvider } from "next-themes";
-import { Geist } from "next/font/google";
+import './globals.css'
+import { Metadata } from 'next'
+import { Toaster } from 'sonner'
+import { ThemeProvider } from 'next-themes'
+import { Geist } from 'next/font/google'
 
-const geist = Geist({ subsets: ["latin"] });
+const geist = Geist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-pdf-support.vercel.app"),
-  title: "REI PDF to Exam Companion",
-  description: "Experimental preview of PDF support with the AI SDK",
-};
+    metadataBase: new URL('https://ai-sdk-preview-pdf-support.vercel.app'),
+    title: 'REI PDF to Exam Companion',
+    description: 'Experimental preview of PDF support with the AI SDK',
+}
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning className={`${geist.className}`}>
-      <body>
-        <ThemeProvider attribute="class" enableSystem forcedTheme="dark">
-          <Toaster position="top-center" richColors />
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html
+            lang="en"
+            suppressHydrationWarning
+            className={`${geist.className}`}
+        >
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    enableSystem
+                    forcedTheme="dark"
+                >
+                    <Toaster position="top-center" richColors />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    )
 }
